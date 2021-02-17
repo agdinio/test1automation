826ca510d72734c245e0f68971b6091404c96ace
Aurelio Dinio <aurelio.dinio@gmail.com>
2021-02-17 15:00:31 +0800

    updated

--- src/test/java/hostcommand/TestIndex.java
+++ src/test/java/hostcommand/TestIndex.java
@@ -9,8 +9,19 @@ public class TestIndex {
 	
 	@Test
 	public void testHostCommand() {
-		System.out.println("Greetings from Manila!");
-		//System.setProperty("webdriver.chrome.driver", "/Volumes/MyBackup/sportoco/javaproject/chromedriver");
+		System.out.println("Greetings from SPORTOCO!" + System.getProperty("os.name"));
+		
+		String chromedriverPath = "uiautomation_lib/chromedriver";			
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+			chromedriverPath = "uiautomation_lib\\chromedriver.exe";			
+		} else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+			chromedriverPath = "uiautomation_lib/chromedriver";			
+		}
+				
+		System.setProperty("webdriver.chrome.driver", chromedriverPath);
+		
+		
+		
 		//System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.29.0");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://gmail.com");		
